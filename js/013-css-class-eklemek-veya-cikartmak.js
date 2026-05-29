22d6353754defc85dc909f799ba79fdbe78c6d2a
Ceren <cerenefe532@gmail.com>
2026-05-29 09:28:06 +0300

    013 guncellendi

--- js/013-css-class-eklemek-veya-cikartmak.js
+++ js/013-css-class-eklemek-veya-cikartmak.js
@@ -3,6 +3,9 @@
 let greeting = document.querySelector("#greeting")
 
 greeting.classList.add("text-primary")
+
+// greeting.style.color = "pink" // Basligi Pembe Yapti
+
 greeting.classList.add("title")
 greeting.classList.add("new-info", "second-class", "third-class") // birden fazla class eklemek
 
