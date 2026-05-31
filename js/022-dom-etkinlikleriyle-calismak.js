dea14607ce5da6a6ec49acc7ff548d40c8e9af3d
Ceren <cerenefe532@gmail.com>
2026-05-31 22:48:11 +0300

    Working with DOM Events

---
+++ js/022-dom-etkinlikleriyle-calismak.js
@@ -0,0 +1,14 @@
+// DOM Etkinlikleriyle Calismak
+// https://www.w3schools.com/jsref/dom_obj_event.asp
+// https://developer.mozilla.org/en-US/docs/Web/Events
+
+
+
+let greeting = document.querySelector("#greeting")
+greeting.addEventListener("click", domClick)
+
+function domClick() {
+    console.log("tiklandi")
+    this.style.color == "red" ? this.style.color = "blue" : this.style.color = "red"
+   
+}
