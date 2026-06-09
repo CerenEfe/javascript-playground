e6cfdd9af3cd932448d124d7a5abcf8907a7893b
Ceren <cerenefe532@gmail.com>
2026-06-10 01:27:57 +0300

    Working with the Fetch API

--- 04-javascript-kategorisi/js/044-fetch-api-ile-calismak.js
+++ 04-javascript-kategorisi/js/044-fetch-api-ile-calismak.js
@@ -2,7 +2,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
 // JSON dosyadan veri cekmek:
-fetch("data/settings.json").then(
+fetch("04-javascript-kategorisi/data/settings.json").then(
   response => response.json()
 ).then(responseJson => {
   console.log(responseJson)
